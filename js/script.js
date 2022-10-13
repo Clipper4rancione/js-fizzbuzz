9f46d3cb8e81df9295ba8855e9f92167b249206e
Clipper4rancione <meluziis.ciro99@gmail.com>
2022-10-13 17:07:49 +0200

    [add] some styles

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
 /* Scrivi un programma che stampi in console i numeri da 1 a 100,
 ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
 Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”. */
+
+const container = document.querySelector('.container');
+const limit = 100;
+const fizz = 'Fizz'
+const buzz = 'Buzz'
+
+//STAMPA DEI NUMERI FINO A 100
+for (let i = 1; i <= limit; i++){
+    //console.log(i);
+    //creazione box
+    const box = document.createElement('div');
+    //aggiunta classe
+    box.className ='box';
+    //scrivo nel box
+    box.innerHTML = i;
+    //aggiunta al container
+    container.append(box);
+
+    //creazione condizione if/else per stampare fizz
+    if(i % 3 === 0){
+        box.innerHTML = fizz;
+        box.classList.add('ligh-blue');
+    }else if(i % 5 === 0){
+        box.innerHTML = buzz;
+        box.classList.add('cr-gray');
+    }else{
+        box.classList.add('d-blue');
+        box.innerHTML = i;
+    }
+}
